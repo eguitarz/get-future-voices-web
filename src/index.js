370ad995294c8dce748429e75b890253c3d46442
Dale Ma <dalema22@gmail.com>
2017-04-29 18:27:53 -0700

    support react-router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { createBrowserHistory } from 'history';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  })
+);
+
+const history = syncHistoryWithStore(createBrowserHistory(), store);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="foo"/>
+        <Route path="bar"/>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
